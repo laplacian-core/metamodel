e5960ebfd5cb395b436f2bfc6cf72b2fc631e60f
iwauo <iwauo.tajima@gmail.com>
2019-03-20 08:46:47 +0900

    load the dependent modules in the plugin

--- src/main/kotlin/laplacian/metamodel/gradle/MetamodelPlugin.kt
+++ src/main/kotlin/laplacian/metamodel/gradle/MetamodelPlugin.kt
@@ -1,13 +1,32 @@
 package laplacian.metamodel.gradle
+import laplacian.gradle.GeneratorPlugin
 import laplacian.gradle.task.LaplacianGenerateExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
 class MetamodelPlugin: Plugin<Project> {
     override fun apply(project: Project) {
-        project.plugins.apply("laplacian.generator")
+        project.plugins.apply(GeneratorPlugin::class.java)
         val extension = project.extensions.getByType(LaplacianGenerateExtension::class.java)
         extension.model {
             modelEntryResolver(MetamodelModelEntryResolver())
         }
+        project.configurations.getByName("implementation").dependencies.addAll(
+            arrayOf(
+                "laplacian:laplacian.generator:1.0.0",
+                "laplacian:laplacian.model.metamodel:1.0.0"
+            ).map{ project.dependencies.create(it) }
+        )
+        project.configurations.getByName("model").dependencies.addAll(
+            arrayOf(
+                "laplacian:laplacian.model.metamodel:1.0.0"
+            ).map{ project.dependencies.create(it) }
+        )
+        project.configurations.getByName("template").dependencies.addAll(
+            arrayOf(
+                "laplacian:laplacian.template.entity.kotlin:1.0.0",
+                "laplacian:laplacian.template.entity.json-schema:1.0.0"
+            ).map{ project.dependencies.create(it) }
+        )
     }
 }
