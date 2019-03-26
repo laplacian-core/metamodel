569e2ecc637024b8479bd1d41a35320fdf3b9939
iwauo <iwauo.tajima@gmail.com>
2019-03-26 22:24:38 +0900

    remove the resources which moved to the plugin module

--- src/test/kotlin/laplacian/model/metamodel/template/assertion.kt
+++
@@ -1,5 +0,0 @@
-package laplacian.model.metamodel.template
-
-import laplacian.model.metamodel.MetamodelTemplateAssertion
-
-val assertion = MetamodelTemplateAssertion().withModel("src/main/resources")
