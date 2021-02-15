6330becb0c9a3618a8ff51fda9a177a11b61ca7f
iwauo <iwauo.tajima@gmail.com>
2021-02-16 01:17:29 +0900

    udpate base files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -111,11 +111,11 @@ file_list() {
 generate() {
   LOCAL_MODULE_REPOSITORY=${LOCAL_MODULE_REPOSITORY:-"$PROJECT_BASE_DIR/../../../mvn-repo"}
   local generator_script="$PROJECT_BASE_DIR/scripts/laplacian-generate.sh"
-  local schema_file_path="$(normalize_path 'model-schema-partial.json')"
+  local schema_file_path="$(normalize_path 'model-schema-full.json')"
   local schema_option=
   if [ -f $schema_file_path ]
   then
-    schema_option="--model-schema $(normalize_path 'model-schema-partial.json')"
+    schema_option="--model-schema $(normalize_path 'model-schema-full.json')"
   fi
   $generator_script ${VERBOSE:+'-v'} \
     --plugin 'laplacian:laplacian.metamodel-plugin:1.0.0' \
